91092d219000f79a87e15cceefd6fbca1b8df92c
knielbo <kln@cas.au.dk>
2020-02-15 10:37:01 +0100

    add fixed size resize

--- preprocessing/simplepreprocessor.py
+++ preprocessing/simplepreprocessor.py
@@ -4,3 +4,12 @@
 import cv2
 
 class SimplePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        # store target image width, height, and interpolation
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def preprocess(self, image):
+        # resize image to fixed size, ignore aspect ratio
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
