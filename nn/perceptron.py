8c434c6d5cf23d3f846dc7b33a61cea8980dd016
knielbo <kln@cas.au.dk>
2020-02-21 13:19:10 +0100

    add training func

--- nn/perceptron.py
+++ nn/perceptron.py
@@ -14,6 +14,30 @@ class Perceptron:
         # apply step function
         return 1 if x > 0 else 0
 
+    def fit(self, X, y, epochs=10):
+        # insert column of 1's to allow trainable bias in W
+        X = np.c_[X, np.ones((X.shape[0]))]
+
+        # loop over epochs
+        for epoch in np.arange(0, epochs):
+            # loop over data points
+            for (x, target) in zip(X, y):
+                # dot product of x and W and pass through act
+                p = self.step(np.dot(x, self.W))
+
+                # only update W if p is not target
+                if p != target:
+                    # determine error
+                    error = p - target
+
+                    # update W
+                    self.W += -self.alpha * error * x
     
+    
+
+
+
+    
+
 
 
