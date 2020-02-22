202185aec903cd8800878463904c2ccad4752ae8
knielbo <kln@cas.au.dk>
2020-02-22 11:28:43 +0100

    add repr method

--- nn/neuralnetwork.py
+++ nn/neuralnetwork.py
@@ -15,9 +15,13 @@ class NeuralNetwork:
         for i in np.arange(0, len(layers) - 2):
             # random init w connecting nodes in each layer
             # add extra node for trainable bias
-            w = .np.random.randn(layers[i] + 1, layers[i + 1] + 1)
+            w = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
             self.W.append(w / np.sqrt(layers[i]))
         
         # for the last two layer the input conenctions need a bias term, but not the output
         w = np.random.randn(layers[-2] + 1, layers[-1]) 
         self.W.append(w / np.sqrt(layers[-2]))
+    
+    def __repr__(self):
+        # construct & return string of neural network architecture
+        return "NeuralNetwork: {}".format("-".join(str(l) for l in self.layers))
