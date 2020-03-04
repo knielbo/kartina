498ee5e04cdb3edc5f705f4cc07f9b61d0ed8112
knielbo <kln@cas.au.dk>
2020-03-04 17:23:06 +0100

    add staticmethod

--- nn/conv/shallownet.py
+++ nn/conv/shallownet.py
@@ -1,4 +1,17 @@
 #!/home/knielbo/virtenvs/cv/bin/python
 """
+ShallowNet architecture:
+    INPUT => CONC => RELU => FC
 
 """
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+
+class ShallowNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # init model + input shape to 'channels last'
