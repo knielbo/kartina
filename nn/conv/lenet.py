568efa87ee4bd48c04d58f3f8460766d57f59813
knielbo <kln@cas.au.dk>
2020-03-16 13:56:11 +0100

    add compact lenet arch

--- nn/conv/lenet.py
+++ nn/conv/lenet.py
@@ -17,9 +17,35 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K
 
 
-
 class LeNet:
     @staticmethod
     def build(width, height, depth, classes):
         # init model
-        model = 
+        model = Sequential()
+        inputShape = (height, width, depth)
+
+        # test for "channels_first" for Theano backend users
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+        
+        # first set of CONV => RELU => POOL
+        model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # second set of CONV => RELU => POOL
+        model.add(Conv2D(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), stides=(2, 2)))
+
+        # FC = RELU
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return model
+        return model
